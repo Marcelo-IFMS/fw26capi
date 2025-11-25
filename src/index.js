f518168d16d264a4decb1c3b0db244a93a1c6bc9
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-11-25 17:44:42 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+ 
+app.set('view engine','ejs')
+app.set('views','./views')
 
-// Define your routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
-});
+require("../rotas/home")(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
